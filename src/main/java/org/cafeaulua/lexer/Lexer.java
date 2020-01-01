4df2f48efcabf6a1aaa1cf599c685ac6a61706ff
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 19:39:05 +1300

    Declare Lexer interface's standard methods

--- src/main/java/org/cafeaulua/lexer/Lexer.java
+++ src/main/java/org/cafeaulua/lexer/Lexer.java
@@ -1,4 +1,10 @@
 package org.cafeaulua.lexer;
 
+import java.io.InputStream;
+import java.nio.file.Path;
+
 public interface Lexer {
+    void parseFromFile(Path filePath);
+    void parseFromString(String source);
+    void parseFromStream(InputStream source);
 }

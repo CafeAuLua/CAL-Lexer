25a27e8dde70306de8ff16e4984dfd76b199d788
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 19:59:28 +1300

    Change return types of Lexer interface to Token arrays

--- src/main/java/org/cafeaulua/lexer/Lexer.java
+++ src/main/java/org/cafeaulua/lexer/Lexer.java
@@ -1,10 +1,12 @@
 package org.cafeaulua.lexer;
 
+import org.cafeaulua.lexer.impl.vm53.Token;
+
 import java.io.InputStream;
 import java.nio.file.Path;
 
 public interface Lexer {
-    void parseFromFile(Path filePath);
-    void parseFromString(String source);
-    void parseFromStream(InputStream source);
+    Token[] parseFromFile(Path filePath);
+    Token[] parseFromString(String source);
+    Token[] parseFromStream(InputStream source);
 }

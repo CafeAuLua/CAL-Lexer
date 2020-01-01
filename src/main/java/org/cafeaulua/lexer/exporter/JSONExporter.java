780c9a2d3486f916a3647762700532eac9050efe
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 20:05:49 +1300

    Create stub CSV and JSON exporter

---
+++ src/main/java/org/cafeaulua/lexer/exporter/JSONExporter.java
@@ -0,0 +1,12 @@
+package org.cafeaulua.lexer.exporter;
+
+import org.cafeaulua.lexer.Exporter;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class JSONExporter implements Exporter {
+    @Override
+    public void export() {
+        // TODO: implement
+        throw new NotImplementedException();
+    }
+}

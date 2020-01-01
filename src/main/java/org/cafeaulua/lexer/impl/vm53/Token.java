2107653c12bd1b599f1d694ed18a2cd1b5fc457e
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 19:58:43 +1300

    Replace generic Token with raw type

--- src/main/java/org/cafeaulua/lexer/impl/vm53/Token.java
+++ src/main/java/org/cafeaulua/lexer/impl/vm53/Token.java
@@ -1,10 +1,10 @@
 package org.cafeaulua.lexer.impl.vm53;
 
-public class Token<T> {
+public class Token {
     public final String typeName;
-    public final T value;
+    public final String value;
 
-    public Token(String typeName, T value) {
+    public Token(String typeName, String value) {
         this.typeName = typeName;
         this.value = value;
     }

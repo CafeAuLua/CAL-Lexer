0d0e29784978115ffdb587271b3fae9a6827a647
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 19:52:49 +1300

    Create constructor and a default value for TokenType enum

--- src/main/java/org/cafeaulua/lexer/impl/vm53/TokenType.java
+++ src/main/java/org/cafeaulua/lexer/impl/vm53/TokenType.java
@@ -1,4 +1,13 @@
 package org.cafeaulua.lexer.impl.vm53;
 
 public enum TokenType {
+    // TODO: list all token names. They don't have to be inline with any spec.
+    INVALID ("invalid", 0);
+
+    public final String name;
+    public final int id;
+    TokenType(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 }

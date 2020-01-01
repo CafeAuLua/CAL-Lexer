cd6c4885d2c713184744946cc81c0e47fc2650b5
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 20:23:14 +1300

    Add tokens to Exporter#export method signature

--- src/main/java/org/cafeaulua/lexer/Exporter.java
+++ src/main/java/org/cafeaulua/lexer/Exporter.java
@@ -1,7 +1,9 @@
 package org.cafeaulua.lexer;
 
+import org.cafeaulua.lexer.impl.vm53.Token;
+
 import java.nio.file.Path;
 
 public interface Exporter {
-    void export(Path exportPath);
+    void export(Token[] tokens, Path exportPath);
 }
--- src/main/java/org/cafeaulua/lexer/exporter/CSVExporter.java
+++ src/main/java/org/cafeaulua/lexer/exporter/CSVExporter.java
@@ -1,13 +1,14 @@
 package org.cafeaulua.lexer.exporter;
 
 import org.cafeaulua.lexer.Exporter;
+import org.cafeaulua.lexer.impl.vm53.Token;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.nio.file.Path;
 
 public class CSVExporter implements Exporter {
     @Override
-    public void export(Path exportPath) {
+    public void export(Token[] tokens, Path exportPath) {
         // TODO: implement
         throw new NotImplementedException();
     }

1cf0eb0e1823e12540c86f95fd4bc543eefcafb3
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-03 14:38:01 +1300

    Optimize imports

--- src/test/java/lexer/vm53/LexerKeywordTest.java
+++ src/test/java/lexer/vm53/LexerKeywordTest.java
@@ -4,19 +4,18 @@ import org.cafeaulua.common.Token;
 import org.cafeaulua.common.vm53.TokenType;
 import org.cafeaulua.lexer.Lexer;
 import org.cafeaulua.lexer.impl.vm53.StandardLexer;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 

3681b6bf371862b80cf170ac03c9ec4b304dbaa4
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-03 14:41:42 +1300

    Plan out string literal tests

--- src/test/java/lexer/vm53/LexerStringLiteralTest.java
+++ src/test/java/lexer/vm53/LexerStringLiteralTest.java
@@ -1,4 +1,10 @@
 package lexer.vm53;
 
 public class LexerStringLiteralTest {
+    // 1 string literal
+    // multi string literal
+    // 1 literal mixed tokens
+    // multi literal mixed tokens
+    // unfinished string literal
+    // unfinished string literal mixed tokens
 }
